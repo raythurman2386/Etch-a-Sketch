747a39ec8fc7f8d369879600e7b5c698a3772394
Raymond Thurman <raymondthurman5@gmail.com>
2019-03-09 16:58:44 -0500

    initial JS to create grid and color boxes in black

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-const mainContainer = document.querySelector(".container");
 const gridSquare = document.querySelector(".gridSquare");
 const button = document.querySelector(".button");
-button.addEventListener("click", createGrid);
 
-function createGrid(v) {
-  let div = document.mainContainer;
-  for (let i = 0; i < v; i++) {
+function createGrid() {
+  const gridSize = 32;
+  let div = document.querySelector(".container");
+  for (let i = 0; i < gridSize; i++) {
     let row = document.createElement("div");
     row.className = "row";
-    for (let x = 1; x <= v; x++) {
+    for (let x = 1; x <= gridSize; x++) {
       let cell = document.createElement("div");
       cell.className = "gridSquare";
-      cell.innerText = i * v + x;
       row.appendChild(cell);
     }
     div.appendChild(row);
   }
 }
+
+function fillBlack() {
+  const squares = document.querySelectorAll(".gridSquare");
+  squares.forEach(square => {
+    square.addEventListener("mouseover", function(e) {
+      e.target.setAttribute("style", "background: black");
+    });
+  });
+}
+
+createGrid();
+fillBlack();
